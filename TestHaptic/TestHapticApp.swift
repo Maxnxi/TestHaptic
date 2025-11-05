c1196dd11e3192fb621e3197f9c831653d33e0f4
Maxnxi <nisq@ya.ru>
2025-11-05 18:57:45 +0700

    Initial Commit

---
+++ TestHaptic/TestHapticApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestHapticApp.swift
+//  TestHaptic
+//
+//  Created by Maksim Ponomarev on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TestHapticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
